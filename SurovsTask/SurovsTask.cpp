0cb1e5961b10c691d550673fca5cebf8c9a56b4f
AVGN Sevich <AVGN Sevich>
2016-07-28 22:28:33 -0700

    1.4

--- SurovsTask/SurovsTask.cpp
+++ SurovsTask/SurovsTask.cpp
@@ -5,14 +5,46 @@
 #include "Item.h"
 using namespace std;
 
+void objCreating()
+{
+	string itmName;
+	int quan;
+	int price;
+	string value;
+	int articul;
+
+	cout << "Welcome to the Easy Items Stock Taking!" << endl;
+	cout << "Name: ";
+	cin >> itmName;
+	cout << "Quantity: ";
+	cin >> quan;
+	cout << "Price: ";
+	cin >> price;
+	cout << "Value: ";
+	cin >> value;
+	cout << "Articul: ";
+	cin >> articul;
+
+
+	Item Item(itmName);
+	Item.setParams(quan, price , value, articul);
+	Item.getParams();
+
+}
+
 int main()
 {
-	Item Masik("Dog");
-	Item Brother("Parents");
-	Masik.setParams(1, 1000, "rubles", 127276);
-	Masik.getParams();
-	Brother.setParams(2, 1, "infinity", 108108);
-	Brother.getParams();
+	string cont;
+
+	objCreating();
+	while (true)
+	{
+		cout << "*****************************************\nDo you want to add a new Item? Y/N: ";
+		cin >> cont;
+		if (cont == "Y" || cont == "y") objCreating();
+		else if (cont == "N" || cont == "n") break;
+		else break;
+	}
 
 	system("pause");
     return 0;
